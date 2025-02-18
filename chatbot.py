dbcea7d68c60e9da647dcafbe304624709265518
Joris Chan <24422312@life.hkbu.edu.hk>
2025-02-18 20:57:20 +0800

    first commit

---
+++ chatbot.py
@@ -0,0 +1,39 @@
+# this file is based on version 13.7 of python telegram chatbot
+# and version 1.26.1 of urllib3
+
+# chatbot.py
+
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+import ConfigParser
+import logging
+
+def main():
+    # Load your token and create an Updater for your Bot
+    config = ConfigParser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=config['TELEGRAM']['ACCESS_TOKEN'], use_context=True)
+    dispatcher = updater.dispatcher
+
+    # You can set this logging module, 
+    so you will know when (and why) things do not work as expected
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    # register a dispatcher to handle message: here we register an echo handler
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+
+def echo(update, context):
+    # here we fetch the text sent by the user and reply with the same message
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("Context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+if __name__ == '__main__':
+    main()
